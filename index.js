09549a713d9f486d4749a64a1384b7cb20875222
Muhammed Shareef <shareefnncr@gmail.com>
2026-05-19 15:47:46 +0530

    Update server bind address to 127.0.0.1

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ const startServer = async () => {
 
     const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, "0.0.0.0", () => {
+    app.listen(PORT, "127.0.0.1", () => {
       console.log(
         `${chalk.green('✓')} ${chalk.blue(
           `Server running on port ${PORT}`
